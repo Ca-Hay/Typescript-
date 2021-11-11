18c6a40feb9456ad3802d06e9df808595470afac
cameron <me@you.com>
2021-11-11 13:12:33 +0000

    minor mistake fixed

--- index.js
+++ index.js
@@ -2,5 +2,7 @@
 exports.__esModule = true;
 var promptSync = require("prompt-sync");
 var prompt = promptSync();
-var answer = prompt('Hello, what is your name? ');
-console.log('Your name is ', answer);
+var name = prompt('What is your name? ');
+var age = prompt('How old are you? ');
+var thefunny = prompt('Where do you live? ');
+console.log('Your name is ', name, ' and you are ', age, 'years old and you live at ', thefunny);
--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import * as promptSync from 'prompt-sync';
 const prompt = promptSync();
 let name: string = prompt('What is your name? ');
-let age: string = prompt('How old are you? ');
+let age: number = prompt('How old are you? ');
 let thefunny: string = prompt('Where do you live? ')
 console.log('Your name is ', name, ' and you are ', age, 'years old and you live at ', thefunny);
