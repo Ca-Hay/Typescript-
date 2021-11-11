1a76102739ef3ffebd2b56325c63444bb3ade5cb
cameron <me@you.com>
2021-11-11 13:14:15 +0000

    the command is what i want to do

--- index.ts
+++ index.ts
@@ -2,5 +2,5 @@ import * as promptSync from 'prompt-sync';
 const prompt = promptSync();
 let name: string = prompt('What is your name? ');
 let age: number = prompt('How old are you? ');
-let thefunny: string = prompt('Where do you live? ')
+let thefunny: string = prompt('What is your postcode? ')
 console.log('Your name is ', name, ' and you are ', age, 'years old and you live at ', thefunny);
