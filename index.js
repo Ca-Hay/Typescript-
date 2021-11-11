f1afb350d1515c55eb87d28ad00cc1fb63bd1053
cameron <me@you.com>
2021-11-11 13:18:40 +0000

    snnjfenjs

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ exports.__esModule = true;
 var promptSync = require("prompt-sync");
 var prompt = promptSync();
 var name = prompt('What is your name? ');
-var age = prompt('How old are you? ');
-var thefunny = prompt('Where do you live? ');
+var age = parseInt(prompt('How old are you? '));
+var thefunny = prompt('What is your address? ');
 console.log('Your name is ', name, ' and you are ', age, 'years old and you live at ', thefunny);
--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import * as promptSync from 'prompt-sync';
 const prompt = promptSync();
 let name: string = prompt('What is your name? ');
-let age: number = prompt('How old are you? ');
-let thefunny: string = prompt('What is your postcode? ')
+let age: number = parseInt(prompt('How old are you? '));
+let thefunny: string = prompt('What is your address? ')
 console.log('Your name is ', name, ' and you are ', age, 'years old and you live at ', thefunny);
