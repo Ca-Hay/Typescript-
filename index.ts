398298158eddbf6e0a4be3addb675a07e0c8f28b
cameron <me@you.com>
2021-11-11 13:08:31 +0000

    Basic Conversation

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import * as promptSync from 'prompt-sync';
 const prompt = promptSync();
-let answer: string = prompt('Hello, what is your name? ');
-console.log('Your name is ', answer);
+let name: string = prompt('What is your name? ');
+let age: string = prompt('How old are you? ');
+let thefunny: string = prompt('Where do you live? ')
+console.log('Your name is ', name, ' and you are ', age, 'years old and you live at ', thefunny);
